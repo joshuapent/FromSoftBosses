185481fd749e55bc65a425fe3bdefa200fd0915e
joshuapent <joshdavidpent@gmail.com>
2023-03-30 13:11:41 -0500

    Models basically done

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+ISHARD = (
+    ('Y', 'Yes'),
+    ('N', 'No'),
+)
+
+class Weakness(models.Model):
+    name: models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Resistance(models.Model):
+    name: models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Game(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=250)
+    boss_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title}, Boss Number: {self.boss_number}"
+
 class Boss(models.Model):
     name = models.CharField(max_length=100)
-    game = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    difficulty = models.IntegerField()
-
-def __str__(self):
-    return f"{self.name} : {self.game}"
+    image = models.CharField(max_length=100)
+    weaknesses = models.ManyToManyField(Weakness)
+    resistances = models.ManyToManyField(Resistance)
+    ishard = models.CharField(
+        max_length=1,
+        choices=ISHARD,
+        default=ISHARD[0][0],
+    )
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return f"{self.name} : {self.game}"
