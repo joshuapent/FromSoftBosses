403999aa5fc55cddcfd8e27e89eb60f1f56e62c1
joshuapent <joshdavidpent@gmail.com>
2023-03-30 23:13:17 -0500

    Added the fromsoft db

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Boss
+from .models import Boss, Game, Weakness, Resistance
 
 # Register your models here.
 admin.site.register(Boss)
+admin.site.register(Game)
+admin.site.register(Weakness)
+admin.site.register(Resistance)
